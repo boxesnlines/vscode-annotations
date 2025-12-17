916e7ecac33fe4ed66cc148323dcf71b8f80e51c
Simon Smart <simon@skedda.com>
2025-12-17 12:43:12 +0000

    Removed author - can re-eadd in the future if we want it.

--- src/records/Annotation.ts
+++ src/records/Annotation.ts
@@ -1,28 +1,25 @@
 import * as vscode from 'vscode';
 
 export class Annotation {
-  constructor(selection: vscode.Selection, text: string, author: string) {
+  constructor(selection: vscode.Selection, text: string) {
     this.selection = selection;
     this.text = text;
-    this.author = author;
   }
   get id(): string {
     return Annotation.GetKeyFromSelection(this.selection);
   }
   selection: vscode.Selection;
   text: string;
-  author: string;
   toJSON() {
     return {
       id: this.id,
       text: this.text,
-      author: this.author
     };
   }
   fromJSON(json: string): Annotation {
     const obj = JSON.parse(json);
     const selection = this.stringToSelection(obj.id);
-    return new Annotation(selection, obj.text, obj.author);
+    return new Annotation(selection, obj.text);
   }
   stringToSelection(id: string): vscode.Selection {
     const [startLine, startChar, endLine, endChar] = id.split(":");
