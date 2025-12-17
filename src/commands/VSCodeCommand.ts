0c40c53f166e408c038657fc958acb64200f954e
Simon Smart <simon@skedda.com>
2025-12-17 12:32:51 +0000

    Removed obsolete stuff.

--- src/commands/VSCodeCommand.ts
+++
@@ -1,9 +0,0 @@
-import * as vscode from 'vscode';
-
-export abstract class VSCodeCommand {
-    readonly id!: string;
-    abstract execute(...args: unknown[]): unknown;
-    registerCommand(context: vscode.ExtensionContext) {
-        context.subscriptions.push(vscode.commands.registerCommand(this.id, ()=>this.execute()));
-    }
-}
